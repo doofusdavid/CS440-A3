c118ade81aa1b6b34cb0e71e998d04b79efef7e0
David Edwards <david.edwards@colostate.edu>
2017-09-29 20:40:01 -0600

    Working.  Time to document

--- A3MySolution.py
+++ A3MySolution.py
@@ -1,6 +1,10 @@
 import math
 import pandas as pd
+from IPython.display import display, display_html
 
+
+# Recursive Best First Search (Figure 3.26, Russell and Norvig)
+#  Recursive Iterative Deepening form of A*, where depth is replaced by f(n)
 
 class Node:
     def __init__(self, state, f=0, g=0, h=0):
@@ -8,17 +12,14 @@ class Node:
         self.f = f
         self.g = g
         self.h = h
-
     def __repr__(self):
         return "Node(" + repr(self.state) + ", f=" + repr(self.f) + \
                ", g=" + repr(self.g) + ", h=" + repr(self.h) + ")"
-
 
 def aStarSearch(startState, actionsF, takeActionF, goalTestF, hF):
     h = hF(startState)
     startNode = Node(state=startState, f=0 + h, g=0, h=h)
     return aStarSearchHelper(startNode, actionsF, takeActionF, goalTestF, hF, float('inf'))
-
 
 def aStarSearchHelper(parentNode, actionsF, takeActionF, goalTestF, hF, fmax):
     if goalTestF(parentNode.state):
@@ -44,47 +45,16 @@ def aStarSearchHelper(parentNode, actionsF, takeActionF, goalTestF, hF, fmax):
         # next lowest f value
         alternativef = children[1].f if len(children) > 1 else float('inf')
         # expand best child, reassign its f value to be returned value
+        global globalDepth
+        globalDepth = min(fmax, alternativef)
+        global globalNodes
+        globalNodes += 1
         result, bestChild.f = aStarSearchHelper(bestChild, actionsF, takeActionF, goalTestF,
                                                 hF, min(fmax, alternativef))
         if result is not "failure":  # g
             result.insert(0, parentNode.state)  # /
             return (result, bestChild.f)  # d
-            #     / \
 
-
-if __name__ == "__main__":  # b   h
-    #   / \
-    successors = {'a': ['b', 'c'],  # a   e
-                  'b': ['d', 'e'],  # \
-                  'c': ['f'],  # c   i
-                  'd': ['g', 'h'],  # \ /
-                  'f': ['i', 'j']}  # f
-
-
-    def actionsF(s):  # \
-        try:  # j
-            ## step cost of each action is 1
-            return [(succ, 1) for succ in successors[s]]
-        except KeyError:
-            return []
-
-
-    def takeActionF(s, a):
-        return a
-
-
-    def goalTestF(s):
-        return s == goal
-
-
-    def h1(s):
-        return 0
-
-
-    start = 'a'
-    goal = 'h'
-    result = aStarSearch(start, actionsF, takeActionF, goalTestF, h1)
-    print('Path from a to h is', result[0], 'for a cost of', result[1])
 
 
 def printState(state):
@@ -140,22 +110,6 @@ def findBlank_8p(state):
     return findBlank(state)
 
 
-def actionsF_8p_generator(state):
-    """
-    Generator originally used to determine valid moves for 0 state.  Deprecated because random puzzle state creation
-    wanted a length, and Python can't get length of a generator.  Replaced by actionsF_8p
-    :param state: current state of the puzzle
-    :return: next valid move for blank state
-    """
-    position = findBlank_8p(state)
-    sqrt = int(math.sqrt(len(state)))
-
-    if position[1] != 0: yield "left"
-    if position[1] != sqrt - 1: yield "right"
-    if position[0] != 0: yield "up"
-    if position[0] != sqrt - 1: yield "down"
-
-
 def actionsF(state):
     """
     Determines valid moves for 0 state in a puzzle.
@@ -168,10 +122,10 @@ def actionsF(state):
     sqrt = int(math.sqrt(len(state)))
 
     actions = []
-    if position[1] != 0: actions.append("left")
-    if position[1] != sqrt - 1: actions.append("right")
-    if position[0] != 0: actions.append("up")
-    if position[0] != sqrt - 1: actions.append("down")
+    if position[1] != 0: actions.append(("left", 1))
+    if position[1] != sqrt - 1: actions.append(("right", 1))
+    if position[0] != 0: actions.append(("up", 1))
+    if position[0] != sqrt - 1: actions.append(("down", 1))
     return actions
 
 
@@ -222,19 +176,19 @@ def legalAction(arrState, action, position, sqrt):
     :param sqrt: what size of a puzzle is it (3 for 8p)
     :return: Whether move is valid or not.
     """
-    if action == "left" and position[1] == 0:
+    if action[0] == "left" and position[1] == 0:
         return False
-    if action == "right" and position[1] == sqrt - 1:
+    if action[0] == "right" and position[1] == sqrt - 1:
         return False
-    if action == "up" and position[0] == 0:
+    if action[0] == "up" and position[0] == 0:
         return False
-    if action == "down" and position[0] == sqrt - 1:
+    if action[0] == "down" and position[0] == sqrt - 1:
         return False
 
     return True
 
 
-def takeActionF(state, action):
+def takeActionF_8p(state, action):
     """
     Moves the blank piece the appropriate direction, and returns the new state.
     :param state: Current state of puzzle
@@ -244,26 +198,17 @@ def takeActionF(state, action):
     import numpy
 
     position = findBlank_8p(state)
-    sqrt = squareOfState(state)
+    sqrt = 3
 
     arrState = numpy.reshape(state, (sqrt, sqrt))
     if not legalAction(arrState, action, position, sqrt):
         return "Could not move that direction"
 
-    if action == "left": return swap(arrState, position, (position[0], position[1] - 1))
-    if action == "right": return swap(arrState, position, (position[0], position[1] + 1))
-    if action == "up": return swap(arrState, position, (position[0] - 1, position[1]))
-    if action == "down": return swap(arrState, position, (position[0] + 1, position[1]))
-
-
-def takeActionF_8p(state, action):
-    """
-    8p specific takeAction function, uses generic square action.
-    :param state: Current state of puzzle
-    :param action: Which direction to move the blank piece
-    :return: New state after the 0 piece has moved.
-    """
-    return takeActionF(state, action)
+    direction = action[0]
+    if direction == "left": return (swap(arrState, position, (position[0], position[1] - 1)), 1)
+    if direction == "right": return (swap(arrState, position, (position[0], position[1] + 1)), 1)
+    if direction == "up": return (swap(arrState, position, (position[0] - 1, position[1])), 1)
+    if direction == "down": return (swap(arrState, position, (position[0] + 1, position[1])), 1)
 
 
 def swap(state, location1, location2):
@@ -301,10 +246,9 @@ def depthLimitedSearch(startState, goalState, actionsF, takeActionF, depthLimit)
         cutoffOccurred = False
 
     for action in actionsF(startState):
-        globalNodes += 1
         childState = takeActionF(startState, action)
-
-        result = depthLimitedSearch(childState, goalState, actionsF, takeActionF, depthLimit - 1)
+        globalNodes += 1
+        result = depthLimitedSearch(childState[0], goalState, actionsF, takeActionF, depthLimit - 1)
 
         if result is 'cutoff':
             cutoffOccurred = True
@@ -383,11 +327,11 @@ def ebf(nNodes, depth, precision=0.01):
     found = False
     midpoint = 0
 
-    guess = nNodes ** (1 / depth)
+    # guess = nNodes ** (1 / depth)
     # return guess
     while first <= last and not found:
         midpoint = (first + last) / 2
-        nPrime = estimateX(midpoint, depth)
+        nPrime = estimateX(midpoint, depth) if midpoint != 1 else 1
         if abs(nPrime - nNodes) < precision:
             found = True
         else:
@@ -406,9 +350,7 @@ def h1_8p(state, goal):
     return 0
 
 def h2_8p(state, goal):
-    statePos = findBlank(state)
-    goalPos = findBlank(goal)
-    return abs(statePos[0] - goalPos[0]) + abs(statePos[1] - goalPos[1])
+    return manhattanDistance(0, state, goal)
 
 
 def manhattanDistance(number, state, goal):
@@ -425,68 +367,123 @@ def h3_8p(state, goal):
     return value
 
 
-def goaltest_8p(state, goal):
+
+def goalTestF_8p(state, goal):
     return state == goal
 
 
-def runIDSTest():
+def runExperiment(goalState1, goalState2, goalState3, heuristic_functions):
+    h1_8p = heuristic_functions[0]
+    h2_8p = heuristic_functions[1]
+    h3_8p = heuristic_functions[2]
+
     global globalDepth
     globalDepth = 0
     global globalNodes
     globalNodes = 0
 
-    idsSolutionPath1 = iterativeDeepeningSearch(startState, goalState1, actionsF_8p, takeActionF, 10)
+    idsSolutionPath1 = iterativeDeepeningSearch(startState, goalState1, actionsF_8p, takeActionF_8p, 10)
     idsDepth1 = globalDepth
     idsNodes1 = globalNodes
 
     globalNodes = 0
-    idsSolutionPath2 = iterativeDeepeningSearch(startState, goalState2, actionsF_8p, takeActionF, 10)
+    idsSolutionPath2 = iterativeDeepeningSearch(startState, goalState2, actionsF_8p, takeActionF_8p, 10)
     idsDepth2 = globalDepth
     idsNodes2 = globalNodes
 
     globalNodes = 0
-    idsSolutionPath3 = iterativeDeepeningSearch(startState, goalState3, actionsF_8p, takeActionF, 15)
+    idsSolutionPath3 = iterativeDeepeningSearch(startState, goalState3, actionsF_8p, takeActionF_8p, 15)
     idsDepth3 = globalDepth
     idsNodes3 = globalNodes
 
     globalNodes = 0
-    astarsolutionpath1 = aStarSearch(startState, actionsF_8p, takeActionF, lambda s: goaltest_8p(s, goalState1),
+    globalDepth = 0
+    astarsolutionpath1 = aStarSearch(startState, actionsF_8p, takeActionF_8p, lambda s: goalTestF_8p(s, goalState1),
                                      lambda s: h1_8p(s, goalState1))
-    astarDepth1 = globalDepth
-    astarNodes1 = globalNodes
+    astar1Depth1 = globalDepth
+    astar1Nodes1 = globalNodes
 
     globalNodes = 0
-    astarsolutionpath1 = aStarSearch(startState, actionsF_8p, takeActionF, lambda s: goaltest_8p(s, goalState1),
+    globalDepth = 0
+    astarsolutionpath1 = aStarSearch(startState, actionsF_8p, takeActionF_8p, lambda s: goalTestF_8p(s, goalState1),
                                      lambda s: h2_8p(s, goalState1))
-    astarDepth2 = globalDepth
-    astarNodes2 = globalNodes
+    astar1Depth2 = globalDepth
+    astar1Nodes2 = globalNodes
 
     globalNodes = 0
-    astarsolutionpath1 = aStarSearch(startState, actionsF_8p, takeActionF, lambda s: goaltest_8p(s, goalState1),
+    globalDepth = 0
+    astarsolutionpath1 = aStarSearch(startState, actionsF_8p, takeActionF_8p, lambda s: goalTestF_8p(s, goalState1),
                                      lambda s: h3_8p(s, goalState1))
-    astarDepth3 = globalDepth
-    astarNodes3 = globalNodes
+    astar1Depth3 = globalDepth
+    astar1Nodes3 = globalNodes
 
-    print("{!s:^40}{!s:^40}{!s:^40}".format(goalState1, goalState2, goalState3))
+    globalNodes = 0
+    globalDepth = 0
+    astarsolutionpath1 = aStarSearch(startState, actionsF_8p, takeActionF_8p, lambda s: goalTestF_8p(s, goalState2),
+                                     lambda s: h1_8p(s, goalState2))
+    astar2Depth1 = globalDepth
+    astar2Nodes1 = globalNodes
+
+    globalNodes = 0
+    globalDepth = 0
+    astarsolutionpath1 = aStarSearch(startState, actionsF_8p, takeActionF_8p, lambda s: goalTestF_8p(s, goalState2),
+                                     lambda s: h2_8p(s, goalState2))
+    astar2Depth2 = globalDepth
+    astar2Nodes2 = globalNodes
+
+    globalNodes = 0
+    globalDepth = 0
+    astarsolutionpath1 = aStarSearch(startState, actionsF_8p, takeActionF_8p, lambda s: goalTestF_8p(s, goalState2),
+                                     lambda s: h3_8p(s, goalState2))
+    astar2Depth3 = globalDepth
+    astar2Nodes3 = globalNodes
+
+    globalNodes = 0
+    globalDepth = 0
+    astarsolutionpath1 = aStarSearch(startState, actionsF_8p, takeActionF_8p, lambda s: goalTestF_8p(s, goalState3),
+                                     lambda s: h1_8p(s, goalState3))
+    astar3Depth1 = globalDepth
+    astar3Nodes1 = globalNodes
+
+    globalNodes = 0
+    globalDepth = 0
+    astarsolutionpath1 = aStarSearch(startState, actionsF_8p, takeActionF_8p, lambda s: goalTestF_8p(s, goalState3),
+                                     lambda s: h2_8p(s, goalState3))
+    astar3Depth2 = globalDepth
+    astar3Nodes2 = globalNodes
+
+    globalNodes = 0
+    globalDepth = 0
+    astarsolutionpath1 = aStarSearch(startState, actionsF_8p, takeActionF_8p, lambda s: goalTestF_8p(s, goalState3),
+                                     lambda s: h3_8p(s, goalState3))
+    astar3Depth3 = globalDepth
+    astar3Nodes3 = globalNodes
+
+    print("{!s:^30}{!s:^30}{!s:^30}".format(goalState1, goalState2, goalState3))
 
     state1DataFrame = pd.DataFrame(
-        [[idsDepth1, idsNodes1, "{0:.3f}".format(ebf(idsNodes1, idsDepth1))], [4, 5, 6], [7, 8, 9]],
-        index=["IDS", "A*h1", "A*h2"], columns=["Depth", "Nodes", "EBF"])
+        [[idsDepth1, idsNodes1, "{0:.3f}".format(ebf(idsNodes1, idsDepth1))],
+         [astar1Depth1, astar1Nodes1, "{0:.3f}".format(ebf(astar1Nodes1, astar1Depth1))],
+         [astar1Depth2, astar1Nodes2, "{0:.3f}".format(ebf(astar1Nodes2, astar1Depth2))],
+         [astar1Depth3, astar1Nodes3, "{0:.3f}".format(ebf(astar1Nodes3, astar1Depth3))]],
+        index=["IDS", "A*h1", "A*h2", "A*h3"], columns=["Depth", "Nodes", "EBF"])
 
     state2DataFrame = pd.DataFrame(
-        [[idsDepth2, idsNodes2, "{0:.3f}".format(ebf(idsNodes2, idsDepth2))], [4, 5, 6], [7, 8, 9]], index=list("   "),
+        [[idsDepth2, idsNodes2, "{0:.3f}".format(ebf(idsNodes2, idsDepth2))],
+         [astar2Depth1, astar2Nodes1, "{0:.3f}".format(ebf(astar2Nodes1, astar2Depth1))],
+         [astar2Depth2, astar2Nodes2, "{0:.3f}".format(ebf(astar2Nodes2, astar2Depth2))],
+         [astar2Depth3, astar2Nodes3, "{0:.3f}".format(ebf(astar2Nodes3, astar2Depth3))]], index=list("    "),
         columns=["Depth", "Nodes", "EBF"])
 
     state3DataFrame = pd.DataFrame(
-        [[idsDepth3, idsNodes3, "{0:.3f}".format(ebf(idsNodes3, idsDepth3))], [4, 5, 6], [7, 8, 9]], index=list("   "),
+        [[idsDepth3, idsNodes3, "{0:.3f}".format(ebf(idsNodes3, idsDepth3))],
+         [astar3Depth1, astar3Nodes1, "{0:.3f}".format(ebf(astar3Nodes1, astar3Depth1))],
+         [astar3Depth2, astar3Nodes2, "{0:.3f}".format(ebf(astar3Nodes2, astar3Depth2))],
+         [astar3Depth3, astar3Nodes3, "{0:.3f}".format(ebf(astar3Nodes3, astar3Depth3))]], index=list("    "),
         columns=["Depth", "Nodes", "EBF"])
     # print(pd.concat([state1DataFrame, state2DataFrame, state3DataFrame], axis=1))
-    print(state1DataFrame)
-    print(state2DataFrame)
-    print(state3DataFrame)
 
-    # # print("\t{:^10}{:^10}{:^10}{:^10}{:^5}{:^10}{:^10}{:^10}".format("Algorithm", "Depth", "Nodes", "EBF", "", "Depth", "Nodes", "EBF"))
-    # print("\t{:>8}{:^13}{:^8}{:>6}{:^8}{:^13}{:^8}{:>6}".format("IDS", idsDepth1, "0", "0.000", "", idsDepth2, "0", "0.000"))
+    display_side_by_side(state1DataFrame, state2DataFrame, state3DataFrame)
 
 
 def printSolution(solutionPath):
@@ -498,6 +495,12 @@ def printSolution(solutionPath):
         printState(solution)
         print()
 
+def display_side_by_side(*args):
+    html_str = ''
+    for df in args:
+        html_str += df.to_html()
+    display_html(html_str.replace('table', 'table style="display:inline;padding-right:70px;padding-left:20px;"'),
+                 raw=True)
 
 
 def printState(state):
@@ -518,4 +521,5 @@ def printState(state):
     print()
 
 
-runIDSTest()
+#
+runExperiment(goalState1, goalState2, goalState3, [h1_8p, h2_8p, h3_8p])
